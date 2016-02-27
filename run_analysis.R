168388d8ce9e2799eaa8d0310658ece702d2f57b
Paul Mollel <paulgid@gmail.com>
2016-02-27 12:44:41 +0100

    updated script

--- run_analysis.R
+++ run_analysis.R
@@ -1,70 +1,81 @@
 
-# You should you need to have the UCI HAR Dataset extracted for the analysis to run. 
+library(reshape2)
+
+download.data = function() {
+    "Checks for data directory and creates one if it doesn't exist"
+    if (!file.exists("data")) {
+        message("Creating data directory")
+        dir.create("data")
+    }
+    if (!file.exists("data/UCI HAR Dataset")) {
+        # download the data
+        fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        zipfile="data/UCI_HAR_data.zip"
+        message("Downloading data")
+        download.file(fileURL, destfile=zipfile, method="curl")
+        unzip(zipfile, exdir="data")
+    }
+}
+
+if (!file.exists('data')){
+ download.data()
+}
 
 
-# loading training data 
-activityLabels = read.table('./activity_labels.txt',header=FALSE);
-subjectTraining = read.table('./train/subject_train.txt',header=FALSE);
-features     = read.table('./features.txt',header=FALSE);
-xTraining    = read.table('./train/X_train.txt',header=FALSE);
-yTraining    = read.table('./train/y_train.txt',header=FALSE);
+# loading data
+# Note that Linux directory escaping is used 
 
-# Assigning names, training data 
-colnames(activityLabels)  = c('activityId','activityType');
-colnames(subjectTraining)  = "subjectId";
-colnames(xTraining)        = features[,2]; 
-colnames(yTraining)        = "activityId";
+activityLabels = read.table('data/UCI\ HAR\ Dataset/activity_labels.txt',header=FALSE);
+features     = read.table('data/UCI\ HAR\ Dataset/features.txt',header=FALSE);
+
+subjectTraining = read.table('data/UCI\ HAR\ Dataset/train/subject_train.txt',header=FALSE);
+xTraining    = read.table('data/UCI\ HAR\ Dataset/train/X_train.txt',header=FALSE);
+yTraining    = read.table('data/UCI\ HAR\ Dataset/train/y_train.txt',header=FALSE);
+
+subjectTesting = read.table('data/UCI\ HAR\ Dataset/test/subject_test.txt',header=FALSE);
+xTesting       = read.table('data/UCI\ HAR\ Dataset/test/X_test.txt',header=FALSE);
+yTesting       = read.table('data/UCI\ HAR\ Dataset/test/y_test.txt',header=FALSE);
+
+# assigning names to tables 
+
+names(subjectTraining) <- "subjectID"
+names(subjectTesting) <- "subjectID"
+names(xTraining) <- features$V2
+names(xTesting) <- features$V2
+names(yTraining) <- "activityId"
+names(yTesting) <- "activityId"
+names(activityLabels)  <-  c('activityId','activityType');
+
 
 # combined training data set
 trainingCombined = cbind(yTraining,subjectTraining,xTraining);
-
-# loading test data 
-subjectTesting = read.table('./test/subject_test.txt',header=FALSE);
-xTesting       = read.table('./test/X_test.txt',header=FALSE);
-yTesting       = read.table('./test/y_test.txt',header=FALSE);
-
-# assigning names, training data 
-colnames(subjectTesting) = "subjectId";
-colnames(xTesting)       = features[,2]; 
-colnames(yTesting)       = "activityId";
 
 # combined testing data set 
 testingCombined = cbind(yTesting,subjectTesting,xTesting);
 
 # combine training and testing
-combinedFinal = rbind(trainingCombined,testingCombined);
+finalData = rbind(trainingCombined,testingCombined);
 
 
-colNames  = colnames(combinedFinal); 
-filtering = (grepl("activity..",colNames) | grepl("subject..",colNames) | grepl("-mean..",colNames) & !grepl("-meanFreq..",colNames) & !grepl("mean..-",colNames) | grepl("-std..",colNames) & !grepl("-std()..-",colNames));
-combinedFinal = combinedFinal[filtering==TRUE];
-combinedFinal = merge(combinedFinal,activityLabels,by='activityId',all.x=TRUE);
-colNames  = colnames(combinedFinal); 
+#get columns containing "mean()" or "std()"
+meanAndStdCols <- grepl("mean\\(\\)", names(finalData)) |
+    grepl("std\\(\\)", names(finalData))
+
+# keep the subjectID and activity columns
+meanAndStdCols[1:2] <- TRUE
+
+# Subset finalData table based on the logicalVector to keep only desired columns
+finalData <- finalData[, meanAndStdCols]
 
 
-# Renaming variables
-for (i in 1:length(colNames)) 
-{
-  colNames[i] = gsub("\\()","",colNames[i])
-  colNames[i] = gsub("-mean","Mean",colNames[i])
-  colNames[i] = gsub("^(f)","Freq",colNames[i])
-  colNames[i] = gsub("-std$","StdDev",colNames[i])
-  colNames[i] = gsub("^(t)","Time",colNames[i])
-  colNames[i] = gsub("([Gg]ravity)","Gravity",colNames[i])
-  colNames[i] = gsub("[Gg]yro","Gyro",colNames[i])
-  colNames[i] = gsub("AccMag","AccMagnitude",colNames[i])
-  colNames[i] = gsub("JerkMag","JerkMagnitude",colNames[i])
-  colNames[i] = gsub("GyroMag","GyroMagnitude",colNames[i])
-  colNames[i] = gsub("([Bb]ody[Bb]ody|[Bb]ody)","Body",colNames[i])
-  colNames[i] = gsub("([Bb]odyaccjerkmag)","BodyAccJerkMagnitude",colNames[i])
-};
+# 3. Use descriptive activity names to name the activities in the data set
 
-# combinedFinal with better variable names 
-colnames(combinedFinal) = colNames;
+# Merge the finalData set with the acitivityType table to include descriptive activity names
+finalData = merge(finalData, activityLabels,by='activityId',all.x=TRUE); 
 
-# Creating Tidy final 
+melted <- melt(finalData, id=c("subjectID","activityType"))
+tidyData <- dcast(melted, subjectID+activityType ~ variable, mean)
 
-WithoutActivity  = combinedFinal[,names(combinedFinal) != 'activityType'];
-tidyFinal    = aggregate(WithoutActivity[,names(WithoutActivity) != c('activityId','subjectId')],by=list(activityId=WithoutActivity$activityId,subjectId = WithoutActivity$subjectId),mean);
-tidyFinal    = merge(tidyFinal,activityType,by='activityId',all.x=TRUE);
-write.table(tidyFinal, './tidyFinal.txt',row.names=TRUE,sep='\t');
+write.table(tidyData, './tidyData.txt',row.names=TRUE,sep='\t');
+
+
